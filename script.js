acd28e51afe35d507ea9fc82abc1f30f11e71cd0
GarryAmasia <garryamasia@outlook.com>
2023-08-02 12:43:08 +1000

    working on filtering price

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 // ========================================================
 const main = document.querySelector(".main");
 const wrapper = document.querySelector(".wrapper");
-// console.log(wrapper);
 
 // ========================================================
 let slider = document.getElementById("myRange");
 let output = document.getElementById("value");
-// console.log(slider);
 
 output.innerHTML = slider.value;
 
-const onChange = (e) => {
-  const price = e.target.value;
-  output.innerHTML = price;
-};
 // ========================================================
 //fetching data
 let data;
-const fetchData = async () => {
+let products;
+const fetchData = async (filter) => {
   wrapper.classList += " wrapper__loading";
-  // console.log(data);
-  if (!data) {
+  if (!products) {
     const response = await fetch(`https://dummyjson.com/products`);
-    const data = await response.json();
-    // console.log(data);
-    const products = data.products;
-    // console.log(products);
-    products.map((product) => {
-      return (main.innerHTML += renderProducts(product));
-    });
+    data = await response.json();
+    products = data.products;
+    // products.map((product) => {
+    //   return (main.innerHTML += renderProducts(product));
+    // });
   }
+  console.log(products);
+
+  // if (filter === "price_low_to_high") {
+  //   console.log(products);
+  //   products.sort((a, b) => b.price - a.price);
+  // }
+
+  products.map((product) => {
+    return (main.innerHTML += renderProducts(product));
+  });
 
   wrapper.classList.remove("wrapper__loading");
+
+  // if (filter === "price_low_to_high") {
+  //   products.sort((a, b) => {
+  //     return a.price - b.price;
+  //   });
+  // }
 };
+// console.log(products);
 
 const renderProducts = (product) => {
   return `<div class="product m-10">
@@ -43,7 +52,7 @@ const renderProducts = (product) => {
     <p>ID: <span class="product__details--id">${product.id}</span></p>
     <p>Title: <span class="product__details--title">${product.title}</span></p>
     <p>Description: <span class="product__details--description">${product.description}</span> </p>
-    <p>Price:$ <span class="product__details--price">${product.price}</span></p>
+    <p>Price: $<span class="product__details--price">${product.price}</span></p>
     <p>Promo: <span class="product__details--discounted-percentage">${product.discountPercentage}</span> %</p>
     <p>Rating: <span class="product__details--rating">${product.rating}</span></p>
     <p>Available: <span class="product__details--stock">${product.stock}</span> units</p>
@@ -51,6 +60,18 @@ const renderProducts = (product) => {
     <p>Category: <span class="product__details--category">${product.category}</span></p>
   </div>
 </div>`;
+};
+
+const optionPicked = (e) => {
+  // console.log(e.target.value);
+  const selectedOption = e.target.value;
+  // console.log(selectedOption);
+  fetchData(selectedOption);
+};
+
+const onChange = (e) => {
+  const price = e.target.value;
+  output.innerHTML = price;
 };
 
 fetchData();
